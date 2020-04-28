929910ff33e48ff2618a6f570f11bb09b05ff2fa
jsilvaroland <joshuasilvaroland@gmail.com>
2020-04-27 18:44:03 -0700

    Add Users table, User model, Users controller, Sessions controller, updated routes, and some backend user auth helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,29 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    private
+    def current_user
+        return nil unless session[:session_token]
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def require_logged_in
+        redirect_to api_session_url unless logged_in? #should this render json instead? like invalid credentials?
+    end
+
+    def login!(user)
+        user.reset_session_token!
+        session[:session_token] = user.session_token
+        @current_user = user
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logout!
+        @current_user.reset_session_token!
+        session[:session_token] = nil
+        @current_user = nil
+    end
 end
---
+++ frontend/bandspace.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+    // const store = configureStore();
+
+    // testing
+    ReactDOM.render(<h1>React is Working</h1>, root);
+    // end testing
+
+    // ReactDOM.render(<Root store={store}/>, root);
+});
