61c1d99ebb138107b9b0a7c2d56e282b91974232
jsilvaroland <joshuasilvaroland@gmail.com>
2020-08-03 13:48:32 -0700

    Comment out ensure_photo and ensure_song validations for seeding

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,6 +1,6 @@
 class Album < ApplicationRecord
     validates :title, :artist_id, presence: true
-    validate :ensure_photo
+    # validate :ensure_photo
 
     has_many :tracks
     belongs_to :artist,
@@ -8,9 +8,9 @@ class Album < ApplicationRecord
 
     has_one_attached :photo
 
-    def ensure_photo
-        unless self.photo.attached?
-            errors[:photo] <<  "must be attached"
-        end
-    end
+    # def ensure_photo
+    #     unless self.photo.attached?
+    #         errors[:photo] <<  "must be attached"
+    #     end
+    # end
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,6 +1,6 @@
 class Track < ApplicationRecord
     validates :title, :artist_id, presence: true
-    validate :ensure_song
+    # validate :ensure_song
 
     belongs_to :album, optional: true
     belongs_to :artist,
@@ -9,9 +9,9 @@ class Track < ApplicationRecord
     has_one_attached :photo
     has_one_attached :song
 
-    def ensure_song
-        unless self.song.attached?
-            errors[:song] <<  "must be attached"
-        end
-    end
+    # def ensure_song
+    #     unless self.song.attached?
+    #         errors[:song] <<  "must be attached"
+    #     end
+    # end
 end
